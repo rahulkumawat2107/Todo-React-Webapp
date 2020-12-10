f47141349eb8a5dab21c20d8bd8a550370ae5a58
Rahul Kumawat <rahul.kumawat2107@gmail.com>
2020-12-10 10:46:27 +0530

    added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Todo from './Todo';
 import TodoList from './TodoList';
 
 function App() {
-  const[inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [filterValue, setfilterValue] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
-  console.log(todos)
+  //console.log("Filer", filterValue);
+
+  useEffect(() => {
+      filterHandler();
+      console.log(filteredTodos);
+  }, [todos, filterValue]);
+
+  const filterHandler = () => {
+    console.log("Filer", filterValue);
+    switch(filterValue) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
 
   return (
     <div className="app">
       <div className="app_body">
         <h1>Todo App</h1>
-          <Todo todos = {todos} inputText={inputText} setInputText = {setInputText} setTodos = {setTodos}/>
+          <Todo todos = {todos} inputText={inputText} setInputText = {setInputText} setTodos = {setTodos} setfilterValue = {setfilterValue}/>
           <ul>  
-            {todos.map((todo) => (
+            {filteredTodos.map((todo) => (
                 <TodoList key={todo.id} text={todo.text} todo = {todo} todos = {todos} setTodos={setTodos} completed={todo.completed}/>
             ))}  
           </ul>
--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Todo.css'
 
-function Todo({todos, inputText, setInputText, setTodos}) {
+function Todo({inputText, setInputText, todos, setTodos, setfilterValue}) {
 
     const inputTextHandler = (e) => {
         setInputText(e.target.value);
@@ -16,12 +16,21 @@ function Todo({todos, inputText, setInputText, setTodos}) {
         setInputText("");
     }
 
+    const optionHandle = (e) => {
+        setfilterValue(e.target.value);
+    }
+
     return (
         <div className="todo_container">
             <form onSubmit={submitTodo}>
                 <input value={inputText} onChange={inputTextHandler} type="text" placeholder="Enter task here"></input>
                 <button type="submit">Submit</button>
             </form>
+            <select onChange={optionHandle}>
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+            </select>
         </div>
     )
 }
